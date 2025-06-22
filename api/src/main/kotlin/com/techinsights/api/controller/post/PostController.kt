7b92b83d4b26d26402d4ab540458195c7fc84751
kitoha <kitoha@daum.net>
2025-06-22 13:50:38 +0900

    [feature/post] PathVariable 추가(빼먹음)

--- api/src/main/kotlin/com/techinsights/api/controller/post/PostController.kt
+++ api/src/main/kotlin/com/techinsights/api/controller/post/PostController.kt
@@ -7,6 +7,7 @@ import com.techinsights.domain.enums.PostSortType
 import com.techinsights.domain.service.post.PostService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
@@ -31,9 +32,9 @@ class PostController(
     )
   }
 
-  @GetMapping("/api/v1/posts/{id}")
-  fun getPostById(id: String): ResponseEntity<PostResponse> {
-    val postDto: PostDto = postService.getPostById(id)
+  @GetMapping("/api/v1/posts/{postId}")
+  fun getPostById(@PathVariable postId: String): ResponseEntity<PostResponse> {
+    val postDto: PostDto = postService.getPostById(postId)
     return ResponseEntity.ok(PostResponse.fromPostDto(postDto))
   }
 
