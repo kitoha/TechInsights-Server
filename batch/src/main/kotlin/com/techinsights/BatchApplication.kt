37e2f2f34676f8d1f3ca60b00c7cb40903dfd64e
kitoha <kitoha@daum.net>
2025-07-07 00:46:12 +0900

    [feature/master] batch main class 패키지 변경

--- batch/src/main/kotlin/com/techinsights/BatchApplication.kt
+++ batch/src/main/kotlin/com/techinsights/BatchApplication.kt
@@ -1,4 +1,4 @@
-package com.techinsights.batch
+package com.techinsights
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.WebApplicationType
@@ -7,9 +7,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder
 import kotlin.system.exitProcess
 
 @SpringBootApplication
-class BatchApplication {
-}
-
+class BatchApplication
 fun main(args: Array<String>) {
   val context = SpringApplicationBuilder(BatchApplication::class.java)
     .web(WebApplicationType.NONE)
