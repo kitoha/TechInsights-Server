159890e92025163ce4dc16ed6c3d21b4bb288c8a
kitoha <kitoha@daum.net>
2025-08-06 15:25:23 +0900

    [master] aws start_server script 수정.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -8,11 +8,11 @@ echo "Starting application using the pre-configured docker-compose file..."
 # (예: image: kitoha/tech-insights-api:202507171200)
 
 # 1. 최신 버전의 이미지를 Docker Hub에서 받아옵니다.
-docker-compose -f docker-compose.prod.yml pull
+docker compose -f docker-compose.prod.yml pull
 
 # 2. 컨테이너를 백그라운드에서 실행합니다.
 # --remove-orphans: docker-compose 파일에서 제거된 서비스의 컨테이너를 삭제합니다.
-docker-compose -f docker-compose.prod.yml up -d --remove-orphans
+docker compose -f docker-compose.prod.yml up -d --remove-orphans
 
 # 3. 사용하지 않는 이전 버전의 Docker 이미지를 정리하여 디스크 공간을 확보합니다.
 docker image prune -f
