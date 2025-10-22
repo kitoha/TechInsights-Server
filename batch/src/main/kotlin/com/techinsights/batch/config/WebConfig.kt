99d932e1ccf2923f2ac92426d64a9bbc3eecc652
kitoha <kitoha@daum.net>
2025-10-22 15:49:18 +0900

    feat: add retry mechanism for WebClient requests
    - Introduce retryFilter to handle 429 Too Many Requests responses.
    - Implement backoff strategy with a maximum of 3 retries and 2 seconds delay.
    - Add delay in processCrawledData method to throttle requests.

--- batch/src/main/kotlin/com/techinsights/batch/config/WebConfig.kt
+++ batch/src/main/kotlin/com/techinsights/batch/config/WebConfig.kt
@@ -2,8 +2,12 @@ package com.techinsights.batch.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import reactor.util.retry.Retry
+import java.time.Duration
 
 @Configuration
 class WebConfig {
@@ -21,7 +25,17 @@ class WebConfig {
             config.defaultCodecs().maxInMemorySize(5 * 1024 * 1024) // 5MB
           }
           .build()
-      )
+      ).filter(retryFilter())
       .build()
+  }
+
+  private fun retryFilter(): ExchangeFilterFunction {
+    return ExchangeFilterFunction { request, next ->
+      next.exchange(request)
+        .retryWhen(
+          Retry.backoff(3, Duration.ofSeconds(2))
+            .filter { it is WebClientResponseException.TooManyRequests }
+        )
+    }
   }
 }
--- batch/src/main/kotlin/com/techinsights/batch/crawling/PostCrawlingServiceImpl.kt
+++ batch/src/main/kotlin/com/techinsights/batch/crawling/PostCrawlingServiceImpl.kt
@@ -3,6 +3,7 @@ package com.techinsights.batch.crawling
 import com.techinsights.batch.parser.BlogParserResolver
 import com.techinsights.domain.dto.company.CompanyDto
 import com.techinsights.domain.dto.post.PostDto
+import kotlinx.coroutines.delay
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
@@ -16,7 +17,9 @@ class PostCrawlingServiceImpl (
   override suspend fun processCrawledData(companyDto: CompanyDto): List<PostDto> {
     val rssContent = fetchRssContent(companyDto.blogUrl)
     val parser = parserResolver.resolve(companyDto.blogUrl)
-    return parser.parseList(companyDto, rssContent)
+    val posts = parser.parseList(companyDto, rssContent)
+    delay(1000)
+    return posts
   }
 
   private suspend fun fetchRssContent(feedUrl: String): String {
